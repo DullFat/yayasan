27c0f5b720d1e9c94433cf536775befb6fcc8377
DullFat <abd.afs39@gmail.com>
2022-09-11 00:29:24 +0700

    Ngebuat Donasi Page dengan swipper

--- assets/js/main.js
+++ assets/js/main.js
@@ -202,6 +202,42 @@
   });
 
   /**
+   * Donasi Slider
+   */
+  new Swiper(".donasi-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120,
+      },
+    },
+  });
+
+  /**
    * Porfolio isotope and filter
    */
   window.addEventListener("load", () => {
