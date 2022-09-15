ff676d01887c1a863f4cb32133aefc756a6ab0e2
DullFat <abd.afs39@gmail.com>
2022-09-15 22:56:17 +0700

    mau push

--- assets/js/main.js
+++ assets/js/main.js
@@ -204,7 +204,7 @@
   /**
    * Donasi Slider
    */
-  new Swiper(".donasi-slider", {
+  new Swiper(".pagination-slider", {
     speed: 400,
     loop: true,
     autoplay: {
@@ -290,34 +290,6 @@
       el: ".swiper-pagination",
       type: "bullets",
       clickable: true,
-    },
-  });
-
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 40,
-      },
-
-      1200: {
-        slidesPerView: 3,
-      },
     },
   });
 
